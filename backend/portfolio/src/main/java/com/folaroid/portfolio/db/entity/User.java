f74d0e4f64f3494bf26b9547c1cc2c2bcc3b7bc6
jongsun1993 <jongsun1993@gmail.com>
2022-10-24 20:36:53 +0900

    [BE] feat: user entity add

---
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/entity/User.java
@@ -0,0 +1,28 @@
+package com.folaroid.portfolio.db.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userNo;
+
+    @Column(length = 60)
+    private String userGithubId;
+
+    @Column(length = 10)
+    private String userName;
+
+    private java.sql.Date userBirth;
+
+    @Column(length = 40)
+    private String userEmail;
+
+    @Column(length = 15)
+    private String userPhone;
+}
