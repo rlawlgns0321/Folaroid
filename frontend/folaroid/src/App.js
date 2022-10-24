7c8c4db0f464cbe3aab6bf43ab218d4ad7728d81
김미애 <miae83@konkuk.ac.kr>
2022-10-24 17:54:42 +0900

    [FE] feat: start-to-if-sentence

--- frontend/folaroid/src/App.js
+++ frontend/folaroid/src/App.js
@@ -1,26 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from '@mui/material/Button';
+import { React } from 'react';
 
 function App() {
+  const name = '미예';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Contained</Button>
-      </header>
-    </div>
+    <>
+      {name === '미애'? (
+      <h1>{name} 안녕</h1>) : (
+        <h2>넌 미애가 아냐</h2>
+      )}
+    </>
   );
 }
 
