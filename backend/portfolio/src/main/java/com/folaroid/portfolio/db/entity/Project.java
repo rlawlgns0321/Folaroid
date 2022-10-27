f9a8542798f2acf087dd10a4ea4756ba28256ab5
이창현 <rover48@hanmail.net>
2022-10-27 15:38:43 +0900

    [BE] feat: Entity Project append

---
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/entity/Project.java
@@ -0,0 +1,37 @@
+package com.folaroid.portfolio.db.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+@Getter
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pjt_no")
+    private Long pjtNo;
+
+    @Column(name = "pf_no")
+    private Long pfNo;
+
+    @Column(name = "pjt_title", length = 100)
+    private String pjtTitle;
+
+    @Column(name = "pjt_subtitle", length = 100)
+    private String pjtSubtitle;
+
+    @Column(name = "pjt_url", length = 2083)
+    private String pjtUrl;
+
+    @Column(name = "pjt_github_url", length = 2083)
+    private String pjtGithubUrl;
+
+    @Column(name = "pjt_star")
+    private Integer pjtStar;
+
+    @Column(name = "pjt_image_location", length = 2083)
+    private String pjtImageLocation;
+}
