2de463cd939967f891309fccbdd8739be77626ad
KimYongHwan <ymin96@naver.com>
2022-10-28 00:15:27 +0900

    [FE] feat: 리액트 라우터 적용

--- frontend/folaroid/src/App.js
+++ frontend/folaroid/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div >
-      <MainPage/>
+      <Route path='/' component={MainPage} exact/>
     </div>
   );
 }
