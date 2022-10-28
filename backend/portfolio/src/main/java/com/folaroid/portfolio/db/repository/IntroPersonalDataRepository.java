c9ee636cfb81df6ec56d08b38d2315f8ce37e10b
이창현 <rover48@hanmail.net>
2022-10-28 14:50:47 +0900

    [BE] feat: IntroPersonalDataRepository Append

---
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/repository/IntroPersonalDataRepository.java
@@ -0,0 +1,8 @@
+package com.folaroid.portfolio.db.repository;
+
+import com.folaroid.portfolio.db.entity.IntroPersonalData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntroPersonalDataRepository extends JpaRepository<IntroPersonalData, Long> {
+
+}
