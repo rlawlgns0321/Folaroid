850b31b6eec2313fcdfe3e075e0167142e53c1fc
이창현 <rover48@hanmail.net>
2022-10-28 14:45:57 +0900

    [BE] feat: IntroCertificationRepository Append

---
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/repository/IntroCertificationRepository.java
@@ -0,0 +1,8 @@
+package com.folaroid.portfolio.db.repository;
+
+import com.folaroid.portfolio.db.entity.IntroCertification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntroCertificationRepository extends JpaRepository<IntroCertification, Long> {
+
+}
