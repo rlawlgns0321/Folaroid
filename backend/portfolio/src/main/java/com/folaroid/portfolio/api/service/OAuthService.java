03004a0fd26e8f4a7ecb18ac4a16ae62ad4de463
kimjihoon <rlawlgns0321@gmail.com>
2022-10-28 13:03:42 +0900

    [BE] style : 주석 변경

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/OAuthService.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/OAuthService.java
@@ -1,5 +1,6 @@
 package com.folaroid.portfolio.api.service;
 
+import com.folaroid.portfolio.db.entity.User;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -20,7 +21,7 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId(); //oAuthService 이름 = Github
         String userAttributeName = userRequest.getClientRegistration().getProviderDetails()
-                                                .getUserInfoEndpoint().getUserNameAttributeName(); //user PK
+                                                .getUserInfoEndpoint().getUserNameAttributeName(); //oAuthService(Github)에서의 unique 필드값
         Map<String, Object> attributes = oAuth2User.getAttributes(); //oAuth 유저 정보
         return null;
     }
