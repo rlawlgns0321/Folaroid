c17bfc778a21993bdb05f3bc0921db7fe915f6a5
kimjihoon <rlawlgns0321@gmail.com>
2022-10-28 12:06:38 +0900

    [BE] feat : OAuth2Service 유저 정보 저장

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/OAuthService.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/OAuthService.java
@@ -7,6 +7,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
@@ -14,7 +16,12 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2UserService userService = new DefaultOAuth2UserService();
-        OAuth2User oAuth2User = userService.loadUser(userRequest);
+        OAuth2User oAuth2User = userService.loadUser(userRequest); //oAuthService(Github)에서 가져온 유저정보 로드
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId(); //oAuthService 이름 = Github
+        String userAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                                                .getUserInfoEndpoint().getUserNameAttributeName(); //user PK
+        Map<String, Object> attributes = oAuth2User.getAttributes(); //oAuth 유저 정보
         return null;
     }
 }
