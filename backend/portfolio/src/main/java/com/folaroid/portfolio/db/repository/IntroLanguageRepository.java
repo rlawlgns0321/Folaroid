8fd4bc7a06b2fc1ba0ec7d76dcaccaf19168500b
이창현 <rover48@hanmail.net>
2022-10-28 14:49:37 +0900

    [BE] feat: IntroLanguageRepository Append

---
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/repository/IntroLanguageRepository.java
@@ -0,0 +1,8 @@
+package com.folaroid.portfolio.db.repository;
+
+import com.folaroid.portfolio.db.entity.IntroLanguage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntroLanguageRepository extends JpaRepository<IntroLanguage, Long> {
+
+}
