3b8d4988887eca6d4f18489e8c71dadb3c64ba88
Kim YongHwan <ymin96@naver.com>
2022-10-28 16:06:38 +0900

    [FE] feat: react konva 라이브러리 추가

--- frontend/folaroid/src/pages/MainPage.js
+++ frontend/folaroid/src/pages/MainPage.js
@@ -1,10 +1,13 @@
+import { css } from '@emotion/css';
 import React from 'react';
+import MainCanvas from '../components/canvas/MainCanvas';
 import Header from '../components/common/Header';
 
 const MainPage = () => {
     return (
         <div>
-            <Header/>
+            <Header />
+            <MainCanvas/>
         </div>
     );
 };
