cdee325f05708f1f12eb84fc7d058a9706610a34
kimjihoon <rlawlgns0321@gmail.com>
2022-10-28 14:14:57 +0900

    [BE] feat : 사용자 관련 링크 관계 설정

--- backend/portfolio/src/main/java/com/folaroid/portfolio/db/entity/IntroArchiving.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/entity/IntroArchiving.java
@@ -13,7 +13,9 @@ public class IntroArchiving {
     //@Column(name = "intro_archiving_no")
     private Long introArchivingNo;
 
-    private Long introNo;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "intro_no")
+    private Intro intro;
 
     private String archivingName; // 사용자 관련 링크 이름
     private String archivingLink; // 사용자 관련 링크
