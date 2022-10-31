65dc1e6dcc8bd1290d0107e1c21ab726a9817a9e
박종선 <jongsun1993@gmail.com>
2022-10-31 10:40:33 +0900

    [BE] feat: login&out add

--- backend/portfolio/src/main/java/com/folaroid/portfolio/config/RedisConfig.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/config/RedisConfig.java
@@ -3,6 +3,9 @@ package com.folaroid.portfolio.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
