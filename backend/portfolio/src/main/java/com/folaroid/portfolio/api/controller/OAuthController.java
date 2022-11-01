a7c2048fb750082241b2e248a34a8f137dc71c8b
kimjihoon <rlawlgns0321@gmail.com>
2022-11-01 11:11:02 +0900

    [BE] style : LoginController -> OAuthController로 이름 수정

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/OAuthController.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/OAuthController.java
@@ -1,6 +1,5 @@
 package com.folaroid.portfolio.api.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login/oauth2/code/github")
-public class LoginController {
+public class OAuthController {
     @GetMapping
     public String getToken(@RequestParam("code") String authorizationCode) {
         System.out.println("===== authorizationCode : " + authorizationCode);
