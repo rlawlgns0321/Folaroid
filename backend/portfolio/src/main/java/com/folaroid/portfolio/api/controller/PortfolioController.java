1e6ea4067ef7e881fbe6f8c5a193fd3733877558
이창현 <rover48@hanmail.net>
2022-11-01 12:34:01 +0900

    [BE] feat: 포트폴리오의 프로젝트 타이틀 API Patch

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/PortfolioController.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/PortfolioController.java
@@ -1,7 +1,9 @@
 package com.folaroid.portfolio.api.controller;
 
 import com.folaroid.portfolio.api.dto.PortfolioDto;
+import com.folaroid.portfolio.api.dto.ProjectDto;
 import com.folaroid.portfolio.api.service.PortfolioService;
+import com.folaroid.portfolio.api.service.ProjectService;
 import com.folaroid.portfolio.db.entity.Portfolio;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class PortfolioController {
     @Autowired
     private final PortfolioService portfolioService;
+    private final ProjectService projectService;
 
     /**
      * 포트폴리오 제작
@@ -62,5 +65,20 @@ public class PortfolioController {
     public ResponseEntity<?> patchPortfolioTemplate(@PathVariable Long pfNo, @RequestBody PortfolioDto.Request portfolioRequest){
         portfolioService.patchPortfolioTemplate(pfNo, portfolioRequest);
         return ResponseEntity.status(200).body(pfNo);
+    }
+
+    /**
+     * 포트폴리오 프로젝트 제목, 부제목 수정
+     */
+    @PatchMapping("{pfNo}/pjt/{pjtNo}")
+    @ApiOperation(value = "포트폴리오의 프로젝트 Title, SubTitle 수정", notes = "포트폴리오의 프로젝트 Title, SubTitle를 수정한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<?> patchProjectTitle(@PathVariable Long pfNo,Long pjtNo, @RequestBody ProjectDto.Request projectRequest){
+        projectService.patchProjectTitle(pfNo, pjtNo, projectRequest);
+        return ResponseEntity.status(200).body(pjtNo);
     }
 }
