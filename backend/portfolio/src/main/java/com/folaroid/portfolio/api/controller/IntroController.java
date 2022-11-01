49c09f2beb0959e6cf1f7cd78351ee820d9cbf85
이창현 <rover48@hanmail.net>
2022-11-01 12:59:35 +0900

    [BE] feat: IntroPersonalData Controller, Service append

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/IntroController.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/IntroController.java
@@ -1,6 +1,17 @@
 package com.folaroid.portfolio.api.controller;
 
+import com.folaroid.portfolio.api.dto.IntroPersonalDataDto;
+import com.folaroid.portfolio.api.service.IntroPersonalDataService;
+import com.folaroid.portfolio.db.entity.IntroPersonalData;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class IntroController {
+    @Autowired
+    private final IntroPersonalDataService introPersonalDataService;
+
+    /**
+     * 자기소개 개인정보 작성
+     */
+    @PostMapping
+    @ApiOperation(value = "자기소개 개인정보 작성", notes = "자기소개의 개인정보를 작성한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity createPersonalData(@RequestBody IntroPersonalDataDto.Request introPersonalDataRequest){
+        IntroPersonalData introPersonalData = introPersonalDataService.createIntroPersonalData(introPersonalDataRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(introPersonalData);
+    }
+
+
 }
