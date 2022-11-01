0dd86eeb9570f511c205a497f7eb78a25c11c7da
kimjihoon <rlawlgns0321@gmail.com>
2022-11-01 11:07:11 +0900

    [BE] feat : loginController 추가

--- backend/portfolio/src/main/java/com/folaroid/portfolio/config/SecurityConfig.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/config/SecurityConfig.java
@@ -5,29 +5,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
+   /* @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //http.httpBasic().disable();
+        http.csrf().disable();
+    }*/
 
     //private final OAuthService oAuthService;
 
     /*public SecurityConfig(OAuthService oAuthService) {
         this.oAuthService = oAuthService;
     }*/
-    @Bean
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests(a -> a
+        http.authorizeRequests(a -> a
                 .antMatchers("/", "/error", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
         )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-                .oauth2Login();
+                .oauth2Login()
+                .and()
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll())
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
         return http.build();
-    }
+    }*/
 }
