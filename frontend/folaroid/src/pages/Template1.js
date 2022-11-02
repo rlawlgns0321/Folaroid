b93e459b55ea867c6fc2b1b6a7f465f8c40da492
김미애 <miae83@konkuk.ac.kr>
2022-11-02 17:46:45 +0900

    [FE]feat: Camera control

--- frontend/folaroid/src/pages/Template1.js
+++ frontend/folaroid/src/pages/Template1.js
@@ -1,7 +1,38 @@
+import * as THREE from 'three';
 import { React, Suspense } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { useGLTF } from '@react-three/drei';
+import { TextureLoader } from 'three';
+
+export const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+//Camera effect
+
+export function Player() {
+    return (
+        <mesh position={[2, -0.5, 0]} receiveShadow scale={0.5}>
+            <sphereGeometry />
+            <meshLambertMaterial color={'pink'} />
+        </mesh>
+    );
+}
+export function Scene(props) {
+    const floor = useLoader(TextureLoader, 'images/grid.jpg');
+    return (
+        <mesh
+            receiveShadow
+            position-y={-1}
+            rotation-x={-Math.PI / 2}
+            scale={100}
+        >
+            <planeGeometry />
+            <meshBasicMaterial map={floor} />
+        </mesh>
+    );
+}
 
 export function House(props) {
     const { nodes, materials } = useGLTF('/models/house.gltf');
@@ -22,10 +53,23 @@ const Template1 = () => {
         <>
             <Canvas>
                 <Suspense fallback={null}>
-                    <OrbitControls />
+                    {/* 부드럽게 마우스 이동 */}
+                    <OrbitControls
+                        enableDamping={true}
+                        maxDistance={40}
+                        minDistance={2}
+                        maxPolarAngle={Math.PI / 2}
+                        minPolarAngle={Math.PI / 4}
+                    />
                     <ambientLight intensity={0.5} />
                     <spotLight position={[10, 15, 10]} angle={0.3} />
-                    <House />
+                    <Scene />
+                    <House position={[-5, 0, 20]} />
+                    <House position={[7, 0, 10]} />
+                    <House position={[-10, 0, 0]} />
+                    <House position={[10, 0, -10]} />
+                    <House position={[-5, 0, -20]} />
+                    <Player />
                 </Suspense>
             </Canvas>
         </>
