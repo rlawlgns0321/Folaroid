3c60e3d1192fb1483dd6d1082c790f24cba90aa0
박종선 <jongsun1993@gmail.com>
2022-11-02 12:31:56 +0900

    [BE] feat: UserController - login, signup, user_info add

--- backend/portfolio/src/main/java/com/folaroid/portfolio/db/repository/UserRepository.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/db/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.folaroid.portfolio.db.repository;
 
 import com.folaroid.portfolio.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserGithubId(String userGithubId);
+
+    @Query("SELECT i from Intro i where i.pfNo IS NULL and i.userNo = :userNo")
+    Long findUserDefaultData(Long userNo);
 
 }
