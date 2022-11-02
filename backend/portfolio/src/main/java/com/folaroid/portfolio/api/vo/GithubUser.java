7b0e439cc78ca34eb793eb652b78c131c9502fd4
kimjihoon <rlawlgns0321@gmail.com>
2022-11-02 16:37:10 +0900

    [BE] feat : Github User Profile Getter, Setter 설정

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/vo/GithubUser.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/vo/GithubUser.java
@@ -1,5 +1,10 @@
 package com.folaroid.portfolio.api.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GithubUser {
     private String login;
     private String id;
