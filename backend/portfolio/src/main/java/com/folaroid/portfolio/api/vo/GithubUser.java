e127217371e080be6fa9a7d2e915ed1d0501e36f
kimjihoon <rlawlgns0321@gmail.com>
2022-11-02 16:34:13 +0900

    [BE] feat : Github User Profile VO 생성

---
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/vo/GithubUser.java
@@ -0,0 +1,36 @@
+package com.folaroid.portfolio.api.vo;
+
+public class GithubUser {
+    private String login;
+    private String id;
+    private String node_id;
+    private String avatarUrl;
+    private String gravatar_id;
+    private String url;
+    private String html_url;
+    private String followers_url;
+    private String following_url;
+    private String gists_url;
+    private String starred_url;
+    private String subscriptions_url;
+    private String organizations_url;
+    private String repos_url;
+    private String events_url;
+    private String received_events_url;
+    private String type;
+    private String site_admin;
+    private String name;
+    private String company;
+    private String blog;
+    private String location;
+    private String email;
+    private String hireable;
+    private String bio;
+    private String twitter_username;
+    private String public_repos;
+    private String public_gists;
+    private String followers;
+    private String following;
+    private String created_at;
+    private String updated_at;
+}
