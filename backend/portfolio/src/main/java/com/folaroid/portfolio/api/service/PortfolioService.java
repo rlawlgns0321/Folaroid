d22c298830b57c9fc351faf2381be5ff5274b7eb
박종선 <jongsun1993@gmail.com>
2022-11-03 15:28:17 +0900

    [BE] feat: portfolio update

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/PortfolioService.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/PortfolioService.java
@@ -1,10 +1,15 @@
 package com.folaroid.portfolio.api.service;
 
 import com.folaroid.portfolio.api.dto.PortfolioDto;
+import com.folaroid.portfolio.api.dto.UserDto;
 import com.folaroid.portfolio.db.entity.Portfolio;
+
+import java.util.List;
 
 public interface PortfolioService {
     Portfolio createPortfolio(PortfolioDto.portfolioRequest portfolioRequest);
     void deletePortfolio(Long pfNo);
     void patchPortfolioTemplate(Long pfNo, PortfolioDto.portfolioRequest portfolioRequest);
+
+    List<PortfolioDto.PortfolioSimpleDto> readSimplePortfolio(UserDto.UserNoReq request);
 }
