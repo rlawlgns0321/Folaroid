741de1fd452213fb8443698f2e0561935fcfe446
이창현 <rover48@hanmail.net>
2022-11-03 16:12:27 +0900

    [BE] feat: REST_API_IntroCareer_Controller_Service_APPEND

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/IntroAwardsService.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/IntroAwardsService.java
@@ -1,6 +1,5 @@
 package com.folaroid.portfolio.api.service;
 
-import com.folaroid.portfolio.api.dto.IntroArchivingDto;
 import com.folaroid.portfolio.api.dto.IntroAwardsDto;
 
 public interface IntroAwardsService {
