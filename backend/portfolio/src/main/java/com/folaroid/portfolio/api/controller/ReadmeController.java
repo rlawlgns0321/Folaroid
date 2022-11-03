14e51e88197f15e0729e90b377b8fe2e82557123
kimjihoon <rlawlgns0321@gmail.com>
2022-11-03 14:11:46 +0900

    [BE] fix : 문단 단위로 readme content를 받도록 수정

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/ReadmeController.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/controller/ReadmeController.java
@@ -4,28 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class ReadmeController {
 
-    public String getMDContent (String urlString) {
+    public ArrayList<String> getMDContent (String urlString) {
         URL url; // The URL to read
         HttpURLConnection conn; // The actual connection to the web page
         BufferedReader rd; // Used to read results from the web page
         String line; // An individual line of the web page HTML
-        String result = ""; // A long string containing all the HTML
+        ArrayList<String> res = new ArrayList<>(); // A long string containing all the HTML
         try {
             url = new URL(urlString);
             conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             while ((line = rd.readLine()) != null) {
-                result += line;
+                res.add(line);
             }
             rd.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
+        return res;
     }
 
 }
