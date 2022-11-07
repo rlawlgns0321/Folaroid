187ba554b1e3927bd650ce22983aeab2bdf04dc1
김미애 <miae83@konkuk.ac.kr>
2022-11-07 11:15:04 +0900

    [FE]feat: add 템플릿2 배경

--- frontend/folaroid/src/pages/Template2.js
+++ frontend/folaroid/src/pages/Template2.js
@@ -1,11 +1,30 @@
-import { Canvas } from '@react-three/fiber';
-import React from 'react';
+import React, { useRef } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+import * as THREE from 'three';
+import { OrbitControls, Stars } from '@react-three/drei';
 
 const Template2 = () => {
     return (
         <>
-            <h1>하이</h1>
-            <Canvas></Canvas>
+            <Canvas
+                style={{
+                    position: 'fixed',
+                    left: '0',
+                    top: '0',
+                    background: 'black',
+                }}
+            >
+                <Stars
+                    radius={300}
+                    depth={60}
+                    count={20000}
+                    factor={7}
+                    saturation={0}
+                    fade={true}
+                />
+                <pointLight position={[10, 10, 10]} />
+            </Canvas>
         </>
     );
 };
