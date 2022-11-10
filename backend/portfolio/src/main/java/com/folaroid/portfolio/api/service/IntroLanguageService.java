7f9bc0d863ecb6bdcaeefc1a3d7023f7e2bd7898
박종선 <jongsun1993@gmail.com>
2022-11-10 16:14:03 +0900

    [BE] feat: IntroLan update

--- backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/IntroLanguageService.java
+++ backend/portfolio/src/main/java/com/folaroid/portfolio/api/service/IntroLanguageService.java
@@ -1,8 +1,6 @@
 package com.folaroid.portfolio.api.service;
 
-import com.folaroid.portfolio.api.dto.IntroDto;
 import com.folaroid.portfolio.api.dto.IntroLanguageDto;
-import com.folaroid.portfolio.db.entity.Intro;
 import com.folaroid.portfolio.db.entity.IntroLanguage;
 import com.folaroid.portfolio.db.repository.IntroLanguageRepository;
 import com.folaroid.portfolio.db.repository.IntroRepository;
@@ -10,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Date;
 import java.util.List;
 
 
@@ -20,13 +17,14 @@ public class IntroLanguageService {
 
     private final IntroLanguageRepository introLanguageRepository;
     private final IntroRepository introRepository;
+
     @Transactional
     public Long save(IntroLanguageDto.introLanguageRequest introLanguageRequest) {
         return introLanguageRepository.save(introLanguageRequest.toEntity(introRepository.findById(introLanguageRequest.getIntroNo()).get())).getIntroLanguageNo();
     }
     @Transactional
     public List<IntroLanguage> find(Long introNo) {
-        return introLanguageRepository.findAllByIntro(introRepository.findById(introNo).get());
+        return introLanguageRepository.findAllByIntroNo(introNo);
     }
     @Transactional
     public void delete(Long introLanguageNo) {
