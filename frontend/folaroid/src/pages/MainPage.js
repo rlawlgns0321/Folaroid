1c9deb6d1c01aaec686d8289a194006f67e0ae30
이창현 <rover48@hanmail.net>
2022-11-15 13:11:40 +0900

    [FE] feat: Main_Page_and_font_append

--- frontend/folaroid/src/pages/MainPage.js
+++ frontend/folaroid/src/pages/MainPage.js
@@ -5,12 +5,21 @@ import MainCanvas from '../components/canvas/MainCanvas';
 // import Header from '../components/common/Header';
 import HeaderContainer from '../containers/header/HeaderContainer';
 import TestPage from './TestPage';
+import "./svg.css";
 
 const MainPage = () => {
     return (
         <TestPage>
             <HeaderContainer />
             {/* <MainCanvas /> */}
+            {/* <link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet"> */}
+
+            <svg viewBox="0 0 1320 300">
+                <text x="50%" y="20%" dy=".35em" text-anchor="middle">
+                    FOLAROID
+                </text>
+            </svg>  
+ 
         </TestPage>
     );
 };
