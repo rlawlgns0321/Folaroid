252533fc2dcb808c84c667375ccd14cfd2a615e1
김미애 <miae83@konkuk.ac.kr>
2022-11-19 01:32:33 +0900

    Template2:merge to tem5

--- frontend/folaroid/src/App.js
+++ frontend/folaroid/src/App.js
@@ -14,7 +14,6 @@ import Template1 from './pages/Template1';
 import Template2 from './pages/Template2';
 import Template3 from './pages/Template3';
 import Template4 from './pages/Template4';
-import Template5 from './pages/Template5';
 import TestPage from './pages/TestPage';
 import InfoModal from './pages/InfoModal';
 import Space from './components/template/space/Space';
@@ -50,7 +49,6 @@ function App() {
                     <Route path="/tem2" element={<Template2 />} />
                     <Route path="/tem3" element={<Template3 />} />
                     <Route path="/tem4" element={<Template4 />} />
-                    <Route path="/tem5" element={<Template5 />} />
                     {/* 자기소개 모달 샘플 */}
                     <Route path="/infoModal" element={<InfoModal />} />
                     <Route path="space" element={<Space />} />
