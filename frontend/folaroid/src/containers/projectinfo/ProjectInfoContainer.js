afa0ad82b41f291c9bd8223489a0c0e41d4376c9
이창현 <rover48@hanmail.net>
2022-11-20 21:31:35 +0900

    [FE] feat: fonts append

--- frontend/folaroid/src/containers/projectinfo/ProjectInfoContainer.js
+++ frontend/folaroid/src/containers/projectinfo/ProjectInfoContainer.js
@@ -14,57 +14,92 @@ const ProjectInfoContainer = () => {
 
     store.addFont({
         fontFamily: 'BlackAndWhitePicture-Regular',
-        url: 'url("/public_assets/fonts/BlackAndWhitePicture-Regular.ttf")',
+        url: '/public_assets/fonts/BlackAndWhitePicture-Regular.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'BlackHanSans-Regular',
+        url: '/public_assets/fonts/BlackHanSans-Regular.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'Dokdo-Regular',
+        url: '/public_assets/fonts/Dokdo-Regular.ttf',
     });
 
     store.addFont({
         fontFamily: 'Dongle-Bold',
-        url: 'url("/public_assets/fonts/Dongle-Bold.ttf")',
+        url: '/public_assets/fonts/Dongle-Bold.ttf',
     });
 
     store.addFont({
         fontFamily: 'Dongle-Light',
-        url: 'url("/public_assets/fonts/Dongle-Bold.ttf")',
+        url: '/public_assets/fonts/Dongle-Bold.ttf',
     });
 
     store.addFont({
         fontFamily: 'Gaegu-Bold',
-        url: 'url("/public_assets/fonts/Gaegu-Bold.ttf")',
+        url: '/public_assets/fonts/Gaegu-Bold.ttf',
     });
-    
+
     store.addFont({
         fontFamily: 'GothicA1-Black',
-        url: 'url("/public_assets/fonts/GothicA1-Black.ttf")',
+        url: '/public_assets/fonts/GothicA1-Black.ttf',
     });
 
     store.addFont({
         fontFamily: 'GowunBatang-Regular',
-        url: 'url("/public_assets/fonts/GowunBatang-Regular.ttf")',
+        url: '/public_assets/fonts/GowunBatang-Regular.ttf',
     });
 
     store.addFont({
         fontFamily: 'Gugi-Regular',
-        url: 'url("/public_assets/fonts/Gugi-Regular.ttf")',
+        url: '/public_assets/fonts/Gugi-Regular.ttf',
     });
 
     store.addFont({
         fontFamily: 'HiMelody-Regular',
-        url: 'url("/public_assets/fonts/HiMelody-Regular.ttf")',
+        url: '/public_assets/fonts/HiMelody-Regular.ttf',
     });
 
     store.addFont({
         fontFamily: 'Jua-Regular',
-        url: 'url("/public_assets/fonts/Jua-Regular.ttf")',
+        url: '/public_assets/fonts/Jua-Regular.ttf',
     });
 
     store.addFont({
         fontFamily: 'KirangHaerang-Regular',
-        url: 'url("/public_assets/fonts/KirangHaerang-Regular.ttf")',
+        url: '/public_assets/fonts/KirangHaerang-Regular.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'NanumBarunpenB',
+        url: '/public_assets/fonts/NanumBarunpenB.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'NanumBarunpenR',
+        url: '/public_assets/fonts/NanumBarunpenR.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'NanumBrush',
+        url: '/public_assets/fonts/NanumBrush.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'NanumMyeongjo',
+        url: '/public_assets/fonts/NanumMyeongjo.ttf',
+    });
+
+    store.addFont({
+        fontFamily: 'NanumPen',
+        url: '/public_assets/fonts/NanumPen.ttf',
     });
 
     store.addFont({
         fontFamily: '나눔손글씨 가람연꽃',
-        url: 'url("/public_assets/fonts/나눔손글씨 가람연꽃.ttf")',
+        url: '/public_assets/fonts/나눔손글씨 가람연꽃.ttf',
     });
 
     const dispatch = useDispatch();
