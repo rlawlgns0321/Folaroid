8047dff31fd2ba7243148d9a119cf4b2dc77f279
김용환 <ymin96@naver.com>
2022-11-20 22:46:49 +0900

    [FE] fix: 폰트 추가 삭제

---
+++ frontend/folaroid/src/components/dialog/ProjectDialog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectDialog = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProjectDialog;
--- frontend/folaroid/src/containers/projectinfo/ProjectInfoContainer.js
+++ frontend/folaroid/src/containers/projectinfo/ProjectInfoContainer.js
@@ -11,62 +11,6 @@ import { getRepoThunk, github } from '../../modules/github';
 
 const ProjectInfoContainer = () => {
     const store = createStore({ key: 'zkx11y_517U965lTjfcT' });
-
-    store.addFont({
-        fontFamily: 'BlackAndWhitePicture-Regular',
-        url: 'url("/public_assets/fonts/BlackAndWhitePicture-Regular.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'Dongle-Bold',
-        url: 'url("/public_assets/fonts/Dongle-Bold.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'Dongle-Light',
-        url: 'url("/public_assets/fonts/Dongle-Bold.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'Gaegu-Bold',
-        url: 'url("/public_assets/fonts/Gaegu-Bold.ttf")',
-    });
-    
-    store.addFont({
-        fontFamily: 'GothicA1-Black',
-        url: 'url("/public_assets/fonts/GothicA1-Black.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'GowunBatang-Regular',
-        url: 'url("/public_assets/fonts/GowunBatang-Regular.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'Gugi-Regular',
-        url: 'url("/public_assets/fonts/Gugi-Regular.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'HiMelody-Regular',
-        url: 'url("/public_assets/fonts/HiMelody-Regular.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'Jua-Regular',
-        url: 'url("/public_assets/fonts/Jua-Regular.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: 'KirangHaerang-Regular',
-        url: 'url("/public_assets/fonts/KirangHaerang-Regular.ttf")',
-    });
-
-    store.addFont({
-        fontFamily: '나눔손글씨 가람연꽃',
-        url: 'url("/public_assets/fonts/나눔손글씨 가람연꽃.ttf")',
-    });
-
     const dispatch = useDispatch();
     const { pfNo, pjtNo } = useParams();
     const [searchParams, setSearchParams] = useSearchParams();
